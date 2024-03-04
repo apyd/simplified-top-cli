1a4b45e5d49c68b7a9d9fa9de557e4ebb82f3127
apyd <arturpydych@gmail.com>
2024-03-04 22:51:59 +0100

    feat: add utils

---
+++ src/utils/index.js
@@ -0,0 +1,12 @@
+export const getTopProcessCommand = (os) => {
+  switch(os) {
+    case 'WINDOWS_NT': {
+      return 'powershell "Get-Process | Sort-Object CPU -Descending | Select-Object -Property Name, CPU, WorkingSet -First 1 | ForEach-Object { $_.Name + \' \' + $_.CPU + \' \' + $_.WorkingSet }"'
+    }
+    default: return 'ps -A -o %cpu,%mem,comm | sort -nr | head -n 1'
+  }
+}
+
+export const transformOutput = (output) => {
+  return `${Math.floor(new Date().getTime() / 1000)} : ${output}`
+}
