c7acdf5af456d890670e6abe14f078538c776e9c
apyd <arturpydych@gmail.com>
2024-03-05 13:23:09 +0100

    refactor: extract logic into modules

--- src/index.js
+++ src/index.js
@@ -1,76 +1,18 @@
 import { type } from 'node:os'
-import { writeFile } from 'node:fs/promises'
 import { fileURLToPath } from 'node:url'
 import { dirname, join } from 'node:path'
-import { spawn } from 'node:child_process'
-import process from 'node:process'
-import readline from 'node:readline'
 
-import { getErrorMessage, getTopProcessCommand, transformOutput } from './utils/index.js'
+import { createTopProcess } from './commands/topProcess.js'
+import { getTopProcessCommand } from './utils/index.js'
 
 import { LOG_FILE_NAME } from './constants/index.js'
-
-let processId, writeFileId
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
+const logFilePath = join(__dirname, '..', LOG_FILE_NAME)
+
 const command = getTopProcessCommand(type())
+const topProcess = await createTopProcess(logFilePath)
 
-const createTopProcess = () => {
-  let output = ''
-
-  const topProcess = (cmd) => {
-    const startTime = Date.now()
-    const top = spawn(cmd, { shell: true })
-
-    top.on('error', (error) => {
-      const errorMessage = getErrorMessage(error)
-      console.error(`Error writing to file: ${errorMessage}`);
-    });
-
-    top.stdout.on('data', async (data) => {
-      const timeElapsed = Date.now() - startTime
-      const nextExecutionRun = timeElapsed > 100 ? 0 : 100 - timeElapsed
-
-      processId = setTimeout(() => topProcess(cmd), nextExecutionRun)
-
-      try {
-        output = transformOutput(data)
-      } catch (error) {
-        const errorMessage = getErrorMessage(error)
-        console.error(`Error transforming output: ${errorMessage}`);
-      }
-
-      readline.cursorTo(process.stdout, 0, 0);
-      readline.clearLine(process.stdout, 0);
-      readline.clearScreenDown(process.stdout);
-
-      process.stdout.write(data);
-    })
-
-    top.stderr.on('data', (data) => {
-      console.error(`Error reading top process data: ${data}`);
-    })
-  }
-
-  writeFileId = setInterval(async() => {
-    try {
-      await writeFile(join(__dirname, '..', LOG_FILE_NAME), output)
-    }
-    catch (error) {
-      const errorMessage = getErrorMessage(error)
-      console.error(`Error writing to file: ${errorMessage}`);
-    }}, 60000)
-
-  return topProcess
-}
-
-const topProcess = createTopProcess()
 topProcess(command)
-
-process.on('SIGINT', () => {
-  clearTimeout(processId)
-  clearInterval(writeFileId)
-  process.exit()
-})
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,5 @@
-import { WINDOWS_NT } from "../constants/index.js"
+import { WINDOWS_NT, WRITE_TIMEOUT } from "../constants/index.js"
+import { writeFile } from 'fs/promises'
 
 export const getTopProcessCommand = (os) => {
   switch(os) {
@@ -16,3 +17,21 @@ export const transformOutput = (output) => {
 export const getErrorMessage = (error) => {
   return error instanceof Error ? error.message : String(error)
 }
+
+export const writeToFile = async (filePath, output) => {
+    try {
+      const { text } = output
+      await writeFile(filePath, text)
+    }
+    catch (error) {
+      const errorMessage = getErrorMessage(error)
+      throw Error(`Error writing to file: ${errorMessage}`);
+    }
+}
+
+export const scheduleWriteToFile = (logFilePath, output) => {
+  setTimeout(async () => {
+    await writeToFile(logFilePath, output);
+    setTimeout(() => scheduleWriteToFile(logFilePath, output), WRITE_TIMEOUT);
+  }, 0);
+};
