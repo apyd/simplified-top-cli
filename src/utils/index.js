81e053d477162433241a4c8834500ce9f5b1289d
apyd <arturpydych@gmail.com>
2024-03-05 13:49:21 +0100

    fix: replace writeFile with appendFile to append new entries into log

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,5 @@
 import { WINDOWS_NT, WRITE_TIMEOUT } from "../constants/index.js"
-import { writeFile } from 'fs/promises'
+import { appendFile } from 'fs/promises'
 
 export const getTopProcessCommand = (os) => {
   switch(os) {
@@ -20,8 +20,7 @@ export const getErrorMessage = (error) => {
 
 export const writeToFile = async (filePath, output) => {
     try {
-      const { text } = output
-      await writeFile(filePath, text)
+      await appendFile(filePath, output)
     }
     catch (error) {
       const errorMessage = getErrorMessage(error)
@@ -30,8 +29,9 @@ export const writeToFile = async (filePath, output) => {
 }
 
 export const scheduleWriteToFile = (logFilePath, output) => {
+  const timeout = output.text ? WRITE_TIMEOUT : 0
   setTimeout(async () => {
-    await writeToFile(logFilePath, output);
-    setTimeout(() => scheduleWriteToFile(logFilePath, output), WRITE_TIMEOUT);
+    timeout && await writeToFile(logFilePath, output.text);
+    setTimeout(() => scheduleWriteToFile(logFilePath, output), timeout);
   }, 0);
 };
